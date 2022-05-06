0d049e70f1d44dd531ac5a705550660f70efc406
CattoNya <marian.petrita12@gmail.com>
2022-05-06 09:18:45 +0300

    animation

--- Main.gd
+++ Main.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 var sound: AudioStream = preload("res://burenyuu_sound_effect.mp3")
 var texture1 = preload("res://neco_sus.png")
-var texture2 = preload("res://neco_sus_1.png")
+var texture2 = preload("res://neco_sus_2.png")
 
 onready var sprite = $Sprite
 onready var audio = $AudioStreamPlayer
+onready var animated_sprite = $AnimatedSprite
 
 func _ready() -> void:
 	audio.set_stream(sound) 
@@ -15,7 +16,7 @@ func _ready() -> void:
 func _process(delta):
 	if Input.is_action_just_pressed("space_key"):
 		audio.play()
-		sprite.set_texture(texture2)
+		animated_sprite.animation = "sus_talk"
 
 func audio_finished():
-	sprite.set_texture(texture1)
+	animated_sprite.animation = "idle"
